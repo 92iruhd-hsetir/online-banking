7f50209cca36efb464c99a162c9b407d95553c33
Ritesh Dhuri <ritesh.dhuri92@gmail.com>
2024-06-14 00:30:51 +0530

    added package, assets, html and js file

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const root = ReactDOM.createRoot(document.getElementById("online_banking"));
+root.render(
+    <React.StrictMode>
+        <div>Online Banking</div>
+    </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
