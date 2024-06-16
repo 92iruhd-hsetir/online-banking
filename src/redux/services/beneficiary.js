1100a7acf491709d58f813274a2dc12df4c734f5
Ritesh Dhuri <ritesh.dhuri92@gmail.com>
2024-06-17 01:13:55 +0530

    calling list after add, delete and update

--- src/redux/services/beneficiary.js
+++ src/redux/services/beneficiary.js
@@ -4,33 +4,38 @@ import config from '../../common/config'
 export const beneficiaryServices = createApi({
     reducerPath: 'beneficiary-services',
     baseQuery: fetchBaseQuery({ baseUrl: `${config.apiBasePath}beneficiary` }),
+    tagTypes: ['beneList'],
     endpoints: (builder) => ({
         getBeneficiaryList: builder.query({
             query: () => ({
                 url: '/list',
                 method: 'GET'
-            })
+            }),
+            providesTags: ['beneList']
         }),
         addBeneficiary: builder.mutation({
             query: (user) => ({
                 url: '/update',
                 method: 'POST',
                 body: user
-            })
+            }),
+            invalidatesTags: ['beneList']
         }),
         updateBeneficiary: builder.mutation({
             query: (user) => ({
                 url: '/update',
                 method: 'POST',
                 body: user
-            })
+            }),
+            invalidatesTags: ['beneList']
         }),
         deleteBeneficiary: builder.mutation({
             query: (user) => ({
                 url: '/delete',
                 method: 'POST',
                 body: user
-            })
+            }),
+            invalidatesTags: ['beneList']
         })
     })
 
